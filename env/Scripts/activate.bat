890a413513495c77aaa6ae8c73e900a8fb072415
dtdung <dungduong241101@gmail.com>
2022-12-09 13:12:47 +0700

    new computer

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\HUSC\phan mem ma nguon mo\V1\env
+set VIRTUAL_ENV=D:\learn\HUSC\phan mem ma nguon mo\V1\env
 
 if not defined PROMPT set PROMPT=$P$G
 
